3a66763f0ef85ccdf9005dab9ed1dbcbf181c37a
santhoshkumar m <santhoshkumar.sm461819@gmail.com>
2025-06-02 18:35:27 +0530

    jenkins

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from DevOps Practice App!');
+  res.send('Hello this is sandy from devops test project!');
 });
 
 app.listen(3000, '0.0.0.0', () => {
